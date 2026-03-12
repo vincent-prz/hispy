319a5c6aa7096081db19bb82ef776b219902168c
Vincent Perez <vincent@manty.eu>
2026-03-12 13:32:37 +0100

    handle runtime errors with ExceptT

--- app/Eval.hs
+++ app/Eval.hs
@@ -1,21 +1,20 @@
-module Eval (eval, evalSE, standardEnv) where
+module Eval (eval, standardEnv, RuntimeError(..)) where
 
 import Control.Monad.State
 import qualified Data.Map as Map
 import Env (Env, standardEnv)
 import Parser (Atom (..), Exp (..))
+import Control.Monad.Except (ExceptT, MonadError (throwError))
 
--- eval with standard Env
 
-evalSE :: Exp -> Exp
-evalSE expr = evalState (eval expr) standardEnv
+newtype RuntimeError = RuntimeError String
 
-eval :: Exp -> State Env Exp
+eval :: Exp -> ExceptT RuntimeError (State Env) Exp
 eval (Atom (ASymbol s)) = do
   env <- get
   case env Map.!? s of
     Just x -> return x
-    Nothing -> error ("unknown variable " ++ s)
+    Nothing -> throwError (RuntimeError ("unknown variable " ++ s))
 eval num@(Atom (ANumber _)) = return num
 eval (List ((Atom (ASymbol "if")) : rest)) = evalIf rest
 eval (List ((Atom (ASymbol "define")) : rest)) = evalDefine rest
@@ -24,21 +23,21 @@ eval (List (func : args)) = do
   evaluatedArgs <- mapM eval args
   case evaluatedFunc of
     Atom (AFunc f) -> return (f evaluatedArgs)
-    _ -> error "callee is not a function"
+    _ -> throwError (RuntimeError "callee is not a function")
 eval _ = error "unhandled case if eval"
 
-evalIf :: [Exp] -> State Env Exp
+evalIf :: [Exp] -> ExceptT RuntimeError (State Env) Exp
 evalIf [test, conseq, alt] = do
   testResult <- eval test
   case testResult of
     Atom (ABool True) -> return conseq
     Atom (ABool False) -> return alt
-    _ -> error "condition evaluated on non boolean"
-evalIf _ = error "malformed conditional"
+    _ -> throwError (RuntimeError "condition evaluated on non boolean")
+evalIf _ = throwError (RuntimeError "malformed conditional")
 
-evalDefine :: [Exp] -> State Env Exp
+evalDefine :: [Exp] -> ExceptT RuntimeError (State Env) Exp
 evalDefine [Atom (ASymbol symbol), expr] = do
   evaluatedExpr <- eval expr
   modify (Map.insert symbol evaluatedExpr)
   return evaluatedExpr
-evalDefine _ = error "malformed definition"
+evalDefine _ = throwError (RuntimeError "malformed definition")
--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,16 @@ module Main where
 
 import Control.Monad.State
 import Env (Env, standardEnv)
-import Eval (eval)
+import Eval (eval, RuntimeError(..))
 import Parser (parse)
 import System.IO (hFlush, stdout)
+import Control.Monad.Except (ExceptT, runExceptT)
 
 -- program = "(begin (define r (+ 5 5)) (* pi (* r r)))"
 
 -- program = "(<= (+ 1 (* 4 4)) 17)"
 
-processInput :: String -> State Env String
+processInput :: String -> ExceptT RuntimeError (State Env) String
 processInput input = case parse input of
   Left err -> return ("Parsing Error: " ++ show err)
   Right expr -> show <$> eval expr
@@ -18,8 +19,10 @@ processInput input = case parse input of
 repl :: Env -> IO ()
 repl env = do
   program <- putStr "> " >> hFlush stdout >> getLine
-  let (output, newEnv) = runState (processInput program) env
-  putStrLn output
+  let (output, newEnv) = runState (runExceptT (processInput program)) env
+  case output of
+    Left (RuntimeError err) -> putStrLn ("Runtime error: " ++ err)
+    Right newExp -> putStrLn newExp
   repl newEnv
 
 main :: IO ()
